2f265a399bbe8029d871794cab2b5cdacba5b8d9
Suhaila419 <163913554+Suhaila419@users.noreply.github.com>
2024-10-25 18:58:36 +0000

    Add files via upload

--- task1.cpp
+++ task1.cpp
@@ -9,7 +9,7 @@ if(n<=1)return n;
 else return FibonacciRecursion(n-1)+FibonacciRecursion(n-2);
 }
 void multiply(vector<vector<ll>>& a, vector<vector<ll>>& b){
-    
+
     vector<vector<ll>>r(2,vector<ll>(2));
     r[0][0]=(a[0][0]*b[0][0]+a[0][1]*b[1][0])%m;
     r[0][1]=(a[0][0]*b[0][1]+a[0][1]*b[1][1])%m;
@@ -24,9 +24,9 @@ vector<vector<ll>> getPower(vector<vector<ll>>& M, int expo){
     while (expo>0) {
         if (expo%2)
             multiply(res,M);
-        
-        multiply(M,M); 
-        expo/=2; 
+
+        multiply(M,M);
+        expo/=2;
     }
     return res;
 }
@@ -44,17 +44,20 @@ int main()
     int n;
     cout << "Enter the position of Fibonacci you want: ";
     cin >> n;
+    char tryAgain;
+    do{
     cout<<"choose the way you want to implement with\n"
         <<"1. Recursion\n"
         <<"2. Matrix Exponentiation\n";
     int choice;
     cin>>choice;
     if(choice==1)
-        cout<<FibonacciRecursion(n);
-    else if(choice==2)
-        cout<< "The Fibonacci number of "<<n
-            <<"th is: " << FibonacciMatrixMaltiplication(n) << endl;
-
-    ll n;cin>>n;
+           cout<< "The Fibonacci number of "<<n<<"th is: " << FibonacciRecursion(n)<< "\n";
+    else if(choice==2){
+            cout<< "The Fibonacci number of "<<n<<"th is: " << FibonacciMatrixMaltiplication(n) << "\n";
+    }
+    cout<<"If you want to rechoice Again press :(y/n)\n";
+    cin>>tryAgain;
+    }while(tryAgain=='y');
     return 0;
 }
